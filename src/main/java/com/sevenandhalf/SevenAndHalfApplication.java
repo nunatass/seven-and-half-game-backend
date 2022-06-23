63f8b8628d3ce66a856839faa480b1ea5c82cc39
Nuno Andrade <nunatass@gmail.com>
2022-06-23 18:49:07 +0100

    project created

---
+++ src/main/java/com/sevenandhalf/SevenAndHalfApplication.java
@@ -0,0 +1,13 @@
+package com.sevenandhalf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenAndHalfApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SevenAndHalfApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sevenandhalf/SevenAndHalfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenandhalf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenAndHalfApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
